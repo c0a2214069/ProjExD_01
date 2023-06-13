60a1f0678d5cc2800c39320fc6aa164530a2c024
c0a2214069 <c0a2214069@edu.teu.ac.jp>
2023-06-13 16:07:21 +0900

    練習第6問　修正

--- pygame_image.py
+++ pygame_image.py
@@ -7,24 +7,26 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    img_rct = bg_img.get_rect()
     koukaton = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(koukaton, True, False)
     kk_img2 = pg.transform.rotozoom(kk_img, 10, 1.0)
     kk_list = [kk_img,kk_img2]
     tmr = 0
-    count = 0
+    count = 1600
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [count, 0])
+        screen.blit(bg_img,[count,0])
+        screen.blit(bg_img,[count-1600,0])
         screen.blit(kk_list[tmr%2],[300,200])
         pg.display.update()
-        count += 1
-        print(count)
-        if count > 1599:
-            count = 0
-        tmr += 1        
-        clock.tick(10)
+        print(tmr)
+        tmr += 1
+        count -= 1
+        if count < 0:
+            count = 1600       
+        clock.tick(100)
 
 
 if __name__ == "__main__":
