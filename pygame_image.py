7675498170c7575259ae35308409316a3fbc708d
c0a2214069 <c0a2214069@edu.teu.ac.jp>
2023-06-13 15:41:13 +0900

    練習第5問

--- pygame_image.py
+++ pygame_image.py
@@ -6,17 +6,19 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
-    koukaton = pg.image.load("ex01-20230613/fig/3.png")
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    koukaton = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(koukaton, True, False)
-    kk_img2 = pg.transform.rotozoom(koukaton, 10, 1.0)
+    kk_img2 = pg.transform.rotozoom(kk_img, 10, 1.0)
     kk_list = [kk_img,kk_img2]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_list[tmr%2],[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
